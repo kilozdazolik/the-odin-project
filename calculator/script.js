24330a7b57f3973d69e6df062b40c9a7988809d6
kilozdazolik <zotyibogyo@gmail.com>
2023-11-06 19:55:14 +0100

    Fixed displaying issues

--- calculator/script.js
+++ calculator/script.js
@@ -1,81 +1,73 @@
 "use strict";
-// code here will run when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  let numbers = document.querySelectorAll(".data-numbers");
-  let operators = document.querySelectorAll(".data-operator");
-  let currentValue = document.querySelector(".display-current");
-  let previousValue = document.querySelector(".display-previous");
-  const equal = document.getElementById("EQUAL");
+let numbers = document.querySelectorAll(".data-numbers");
+let operators = document.querySelectorAll(".data-operator");
+let currentValue = document.querySelector(".display-current");
+let previousValue = document.querySelector(".display-previous");
+const equal = document.getElementById("EQUAL");
 
-  // variables
-  let firstNum = "";
-  let secondNum = "";
-  let operator = "";
+// variables
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 
-  // functions that handles the math logic
-  function add(a, b) {
-    return a + b;
+// functions that handles the math logic
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, op) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
   }
+}
 
-  function subtract(a, b) {
-    return a - b;
+function handleNum(num) {
+  if (firstNum.length <= 12) {
+    firstNum += num;
   }
+}
 
-  function multiply(a, b) {
-    return a * b;
-  }
+function handleOp(operandor) {
+  operator = operandor;
+  secondNum = firstNum;
+  firstNum = "";
+}
 
-  function divide(a, b) {
-    return a / b;
-  }
-
-  function operate(a, b, op) {
-    switch (op) {
-      case "+":
-        return add(a, b);
-      case "-":
-        return subtract(a, b);
-      case "*":
-        return multiply(a, b);
-      case "/":
-        return divide(a, b);
-    }
-  }
-  //TODO: Fix displaying issues
-  function handleNum(num) {
-    if (operator) {
-      currentValue.textContent = "";
-      operator = "";
-    }
-
-    let clickedNumber = parseFloat(num.textContent);
-    firstNum += clickedNumber;
+numbers.forEach(function (num) {
+  num.addEventListener("click", function (e) {
+    handleNum(e.target.textContent);
     currentValue.textContent = firstNum;
-  }
-
-  function handleOp(operandor) {
-    if (!operator) {
-      operator = operandor.textContent;
-      previousValue.textContent = parseFloat(currentValue.textContent);
-      secondNum = parseFloat(previousValue.textContent);
-      currentValue.textContent += operandor.textContent;
-    }
-  }
-
-  numbers.forEach(function (num) {
-    num.addEventListener("click", function () {
-      handleNum(num);
-    });
   });
+});
 
-  operators.forEach(function (operandor) {
-    operandor.addEventListener("click", function () {
-      handleOp(operandor);
-    });
+operators.forEach(function (operandor) {
+  operandor.addEventListener("click", function (e) {
+    handleOp(e.target.textContent);
+    previousValue.textContent = secondNum + " " + operator;
+    currentValue.textContent = firstNum;
   });
+});
 
-  equal.addEventListener("click", function () {
-    previousValue.textContent = "";
-    currentValue.textContent = operate(firstNum, secondNum, operator);
-  });
+equal.addEventListener("click", function () {
+  previousValue.textContent = "";
+  currentValue.textContent = operate(firstNum, secondNum, operator);
 });
