1113e63b5e1db79b06aa115e15bd6b26729f7aac
kilozdazolik <zotyibogyo@gmail.com>
2023-11-06 09:29:01 +0100

    project restart

--- calculator/script.js
+++ calculator/script.js
@@ -1,36 +1,81 @@
 "use strict";
+// code here will run when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  let numbers = document.querySelectorAll(".data-numbers");
+  let operators = document.querySelectorAll(".data-operator");
+  let currentValue = document.querySelector(".display-current");
+  let previousValue = document.querySelector(".display-previous");
+  const equal = document.getElementById("EQUAL");
 
-function add(a, b) {
-  return a + b;
-}
+  // variables
+  let firstNum = "";
+  let secondNum = "";
+  let operator = "";
 
-function subtract(a, b) {
-  return a - b;
-}
+  // functions that handles the math logic
+  function add(a, b) {
+    return a + b;
+  }
 
-function multiply(a, b) {
-  return a * b;
-}
+  function subtract(a, b) {
+    return a - b;
+  }
 
-function divide(a, b) {
-  return a / b;
-}
+  function multiply(a, b) {
+    return a * b;
+  }
 
-const numbers = document.querySelectorAll(".data-number");
-const operands = document.querySelectorAll(".data-operand");
-const displayFirstNum = document.getElementById("display-first");
-const displaySecondNum = document.getElementById("display-second");
+  function divide(a, b) {
+    return a / b;
+  }
 
-numbers.forEach(function (currentNum) {
-  currentNum.addEventListener("click", function () {
-    displayFirstNum.textContent += currentNum.textContent;
-    console.log("clicked");
+  function operate(a, b, op) {
+    switch (op) {
+      case "+":
+        return add(a, b);
+      case "-":
+        return subtract(a, b);
+      case "*":
+        return multiply(a, b);
+      case "/":
+        return divide(a, b);
+    }
+  }
+  //TODO: Fix displaying issues
+  function handleNum(num) {
+    if (operator) {
+      currentValue.textContent = "";
+      operator = "";
+    }
+
+    let clickedNumber = parseFloat(num.textContent);
+    firstNum += clickedNumber;
+    currentValue.textContent = firstNum;
+  }
+
+  function handleOp(operandor) {
+    if (!operator) {
+      operator = operandor.textContent;
+      previousValue.textContent = parseFloat(currentValue.textContent);
+      secondNum = parseFloat(previousValue.textContent);
+      currentValue.textContent += operandor.textContent;
+    }
+  }
+
+  numbers.forEach(function (num) {
+    num.addEventListener("click", function () {
+      handleNum(num);
+    });
   });
-});
 
-operands.forEach(function (currentOperand) {
-  currentOperand.addEventListener("click", function () {
-    displaySecondNum.textContent = displayFirstNum.textContent;
-    displayFirstNum.textContent = "";
+  operators.forEach(function (operandor) {
+    operandor.addEventListener("click", function () {
+      handleOp(operandor);
+    });
+  });
+
+  equal.addEventListener("click", function () {
+    previousValue.textContent = "";
+    currentValue.textContent = operate(firstNum, secondNum, operator);
   });
 });
