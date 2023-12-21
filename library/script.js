e8af96043d44dc36adf9663c00b54710dab9e93e
kilozdazolik <zotyibogyo@gmail.com>
2023-12-21 15:30:03 +0100

    start of project

---
+++ library/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {}
