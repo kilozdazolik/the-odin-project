5dc5816031ac050ddc20b90cf67eb13588fe88e1
kilozdazolik <zotyibogyo@gmail.com>
2024-01-03 15:33:18 +0100

    fixed rating

--- library/script.js
+++ library/script.js
@@ -6,6 +6,7 @@ const showButton = document.getElementById("showDialog");
 const confirmBtn = document.getElementById("confirmBtn");
 const rating = document.querySelector(".rating");
 const readCheckbox = document.getElementById("read");
+const fileInput = document.getElementById("fileInput");
 
 const myLibrary = [];
 
@@ -33,22 +34,26 @@ function addBookToLibrary(event) {
 }
 
 function displayBooks(title, author, rating) {
-  const starIcons = Array.from(
-    { length: rating },
-    (_, index) =>
-      `              <svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="currentColor"
-  class="w-6 h-6 rating-icon"
->
-  <path
-    fill-rule="evenodd"
-    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-    clip-rule="evenodd"
-  />
-</svg>`
-  ).join("");
+  const starIcons = Array.from({ length: 5 }, (_, index) => {
+    const fillColor =
+      index < rating
+        ? "var(--primary-color-dark)"
+        : "var(--default-color-very-light)";
+    return `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="${fillColor}"
+          class="w-6 h-6 rating-icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      `;
+  }).join("");
 
   const bookHTML = `
   <div class="book">
@@ -63,9 +68,11 @@ function displayBooks(title, author, rating) {
               <h3 class="heading-tertiary">${title}</h3>
               <p class="book-author">${author}</p>
             </div>
-            <div class="book-rating">
-            ${starIcons}
-            </div>
+            ${
+              readCheckbox.checked
+                ? `<div class="book-rating">${starIcons}</div>`
+                : ""
+            }
           </div>
         </div>
   `;
@@ -98,6 +105,12 @@ function getRating() {
   const clickedStars = document.querySelectorAll(".star.clicked").length;
   return clickedStars;
 }
+
+fileInput.addEventListener("change", () => {
+  leftContainer.innerHTML = `<img src="${URL.createObjectURL(
+    fileInput.files[0]
+  )}" alt="Uploaded Image" class="uploaded-img" />`;
+});
 
 readCheckbox.addEventListener("change", toggleRatingVisibility);
 
