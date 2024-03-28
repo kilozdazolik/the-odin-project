81d41f70682dd19a50209f6081efa35144691cc9
kilozdazolik <zotyibogyo@gmail.com>
2024-03-28 19:20:59 +0100

    fixed player assign

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -20,20 +20,23 @@ const Gameboard = (() => {
   return { render };
 })();
 
-const Player = ((name, mark) => {
-  return {
-    name,
-    mark,
-  };
-})();
+const getPlayer = (name, mark) => {
+  return { name, mark };
+};
 
 const Game = (() => {
+  let players = [];
   let currentPlayer;
+  let gameOver;
 
   const start = () => {
-    //TODO: After the pressing the start game assign players, and then
-    // render the game board.
+    players = [
+      getPlayer(player1Input.value, "X"),
+      getPlayer(player2Input.value, "O"),
+    ];
 
+    currentPlayer = 0;
+    gameOver = false;
     Gameboard.render();
   };
   return {
@@ -45,3 +48,5 @@ startButton.addEventListener("click", () => {
   // Draw the gameboard
   Game.start();
 });
+
+//TODO: Handle click events
