c101d38d88e3cfbb1d0938573a34a9a0b92d07f9
kilozdazolik <paulsonzoltan@protonmail.com>
2026-03-17 19:38:25 +0100

    feat: add click event listeners for player grids to log cell coordinates

--- battleship/src/main.js
+++ battleship/src/main.js
@@ -2,3 +2,19 @@ import {UserInterface} from "./userInterface.js";
 
 const ui = new UserInterface();
 ui.createBoard();
+
+const playerOneGrid = document.querySelector(".player-one");
+playerOneGrid.addEventListener("click", (event) => {
+    const coordinates = ui.getCellCoordinates(event);
+    if (coordinates) {
+        console.log("Player One clicked on:", coordinates);
+    }
+});
+
+const playerTwoGrid = document.querySelector(".player-two");
+playerTwoGrid.addEventListener("click", (event) => {
+    const coordinates = ui.getCellCoordinates(event);
+    if (coordinates) {
+        console.log("Player Two clicked on:", coordinates);
+    }   
+});
